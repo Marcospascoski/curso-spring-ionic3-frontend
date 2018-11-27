2b673650e41976287fc1728b1b96308aad4c3e93
Marcos Pascoski <marcospascoski@gmail.com>
2018-11-26 22:10:37 -0300

    Salvando o pedido

--- src/pages/ordem-confirmation/ordem-confirmation.module.ts
+++ src/pages/ordem-confirmation/ordem-confirmation.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { OrdemConfirmationPage } from './ordem-confirmation';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @NgModule({
   declarations: [
@@ -9,5 +10,8 @@ import { OrdemConfirmationPage } from './ordem-confirmation';
   imports: [
     IonicPageModule.forChild(OrdemConfirmationPage),
   ],
+  providers:[
+    PedidoService
+  ]
 })
 export class OrdemConfirmationPageModule {}
--- src/pages/ordem-confirmation/ordem-confirmation.ts
+++ src/pages/ordem-confirmation/ordem-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 /**
  * Generated class for the OrdemConfirmationPage page.
@@ -30,7 +31,8 @@ export class OrdemConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
     
       this.pedido = this.navParams.get('pedido');
   }
@@ -54,6 +56,23 @@ export class OrdemConfirmationPage {
 
   total() : number{
     return this.cartService.total();
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+    .subscribe(response =>{
+      this.cartService.createOrClearCart();
+      console.log(response.headers.get('location'));
+    },
+    error => {
+      if(error.status == 403){
+        this.navCtrl.setRoot('HomePage');
+      }
+    })
   }
 
 }
