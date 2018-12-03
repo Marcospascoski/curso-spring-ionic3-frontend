11b40c82feae2c9f33eb7ba5fe4a7c63b5715f38
Marcos Pascoski <marcospascoski@gmail.com>
2018-12-02 23:52:48 -0300

    Bugfix - atualizacao da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -15,6 +16,7 @@ export class ProfilePage {
 
   cliente: ClienteDTO;
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
   constructor(
@@ -22,7 +24,10 @@ export class ProfilePage {
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera) {
+    public camera: Camera,
+    public sanitizer: DomSanitizer) {
+
+      this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -52,8 +57,23 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
       .subscribe(response => {
         this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+        this.blobToDataURL(response).then(dataUrl =>{
+          let str: string = dataUrl as string;
+          this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+        })
       },
-        error => { });
+        error => { 
+          this.profileImage = 'assets/imgs/avatar-blank.png';
+        });
+  }
+
+  blobToDataURL(blob){
+    return new Promise((fulfill, reject) =>{
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+    });
   }
 
   getCameraPicture() {
@@ -62,7 +82,7 @@ export class ProfilePage {
 
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
@@ -82,7 +102,7 @@ export class ProfilePage {
     const options: CameraOptions = {
       quality: 100,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
@@ -99,7 +119,7 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture)
     .subscribe(reponse => {
       this.picture = null;
-      this.loadData();
+      this.getImageIfExists();
     },
     error =>{
     });
